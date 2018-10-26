e1f7070ada2fb364b62ec59ac2984ac1b81f599c
SLippa91 <slippa91@gmail.com>
2018-10-26 16:38:48 -0500

    initial setup

---
+++ keys.js
@@ -0,0 +1,17 @@
+
+console.log("keys.js is loaded")
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+// const spotify = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
+
+// module.exports = {
+//   spotify: spotify
+// };
+
